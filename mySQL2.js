a3dd4ffadec30c21dd747dc643fae1b81b975ffd
juliop11 <julioperezobrero@hotmail.com>
2023-03-15 15:07:44 +0100

    dia2

---
+++ mySQL2.js
@@ -0,0 +1,19 @@
+let mysql = require("mysql2");
+
+let connection = mysql.createConnection({
+
+    host       :"localhost",
+    user       :"root",
+    password   :"Puertollano1",
+    database   :"dia1"
+})
+
+
+connection.connect(function(error){
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log("Conexion directa");
+    }
+});
